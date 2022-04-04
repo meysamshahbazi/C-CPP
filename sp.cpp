6d24d9296df89b3de20417e3e860b96e0bdb1765
meysam <mysm.shahbazi@gmail.com>
2022-04-04 10:49:52 +0430

    simple pointer

---
+++ sp.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    int num {10};
+
+    cout << "Value of num is: " << num << endl;		
+    cout << "sizeof of num is: " << sizeof num << endl; 	
+    cout << "Address of num is: " << &num << endl;	 
+
+    return 0;
+
+
+}
