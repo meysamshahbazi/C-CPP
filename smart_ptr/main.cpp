4e599f98636f12e8b100d128fb8e2bec61f7433e
meysam <mysm.shahbazi@gmail.com>
2022-05-31 13:42:10 -0400

    add usefule files class

--- smart_ptr/main.cpp
+++ smart_ptr/main.cpp
@@ -2,8 +2,24 @@
 #include <iostream>
 #include <vector>
 
+#include "Account.h"
+#include "Checking_Account.h"
+#include "Trust_Account.h"
+#include "Savings_Account.h"
 
 using namespace std;
+
+
+class Test {
+private:
+    int data;
+public:
+    Test() : data{0} { std::cout << "Test constructor (" << data << ")" << std::endl; }
+    Test(int data) : data {data} { std::cout << "Test constructor (" << data << ")" << std::endl; }
+    int get_data() const {return data; }
+    ~Test() {std::cout << "Test destructor (" << data << ")" << std::endl; }
+};
+
 
 
 int main( int argc, const char ** argv)
