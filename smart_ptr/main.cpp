8c9ee8066208dd2b91ab490113de19c2060868ae
meysam <mysm.shahbazi@gmail.com>
2022-06-02 10:52:14 -0400

    shared ponter

--- smart_ptr/main.cpp
+++ smart_ptr/main.cpp
@@ -22,9 +22,15 @@ public:
 };
 
 
+void func(std::shared_ptr<Test> p) {
+    std::cout << "Use count: " << p.use_count() << std::endl;
+}
+
 
 int main( int argc, const char ** argv)
 {
+    // uniqe pointer
+    /*
     Test * t1 = new Test{100};
     delete t1;
 
@@ -34,7 +40,7 @@ int main( int argc, const char ** argv)
 
     unique_ptr<Test> ptr3;
     ptr3 = move(ptr1);
-
+    
     if (ptr1 == nullptr)
         cout<<"ptr1 is null out\n";
 
@@ -55,10 +61,62 @@ int main( int argc, const char ** argv)
     {
         cout<<*a<<endl;
     }
-
+    
     cout<<"______________________________________\n";
+   */
+    // shared pointer 
+    
+    shared_ptr<int> ptr1{new int {100} };
+    cout<<"ptr1 use_count: "<<ptr1.use_count()<< endl;
+    shared_ptr<int> ptr2 {ptr1};
+    // func(ptr1);
 
+    cout<<"ptr1 use_count: "<<ptr1.use_count()<< endl;
+    cout<<"ptr2 use_count: "<<ptr2.use_count()<< endl;
+
+    ptr1.reset();
+
+    cout<<"ptr1 use_count: "<<ptr1.use_count()<< endl;
+    cout<<"ptr2 use_count: "<<ptr2.use_count()<< endl;
+
+    cout<<"ptr1 value: "<<ptr1<<endl;
+    cout<<"ptr2 value: "<<ptr2<<endl;
+
+    std::cout << "\n==========================================" << std::endl;
+
+    shared_ptr<Test> test_ptr = make_shared<Test>(100);
+    func(test_ptr);
+    std::cout << "Use count: " << test_ptr.use_count() << std::endl;
+    
+    {
+        shared_ptr<Test> test_ptr1 = test_ptr;
+        std::cout << "Use count: " << test_ptr1.use_count() << std::endl;
+        test_ptr.reset();
+
+        
+    }
+
+    std::cout << "Use count: " << test_ptr.use_count() << std::endl;
+    std::cout << "\n==========================================" << std::endl;
+    
+    std::shared_ptr<Account> acc1 = std::make_shared<Trust_Account>("Larry", 10000, 3.1);
+    std::shared_ptr<Account> acc2 = std::make_shared<Checking_Account>("Moe", 5000);
+    std::shared_ptr<Account> acc3 = std::make_shared<Savings_Account>("Curly", 6000);
+
+    std::vector<std::shared_ptr<Account>> accounts;
+    accounts.push_back(acc1);
+    accounts.push_back(acc2);
+    accounts.push_back(acc3);
     
 
+    for (const auto &acc: accounts) {
+        std::cout << *acc << std::endl;
+        std::cout << "Use count: " << acc.use_count() << std::endl;
+    }
+
+    std::cout << "\n==========================================" << std::endl;
+    
+
+    // wake pointer
     return 0;
 }
