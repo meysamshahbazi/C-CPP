e9836550e3dd076b67d75cdc89ae0539cfea5946
meysam <mysm.shahbazi@gmail.com>
2022-06-04 14:20:45 -0400

    algorithms

--- stl/main.cpp
+++ stl/main.cpp
@@ -5,6 +5,9 @@
 #include <map>
 #include <set>
 #include <list>
+#include <algorithm>
+#include <cctype>
+
 
  
 
@@ -34,6 +37,23 @@ void my_swap(T &a, T &b)
     bool operator<(const Person & rhs) const
     {
         return this->age < rhs.age;
+    }
+ };
+
+ class Personn{
+    std::string name;
+    int age;
+public:
+    Personn() = default;
+    Personn(std::string name, int age)
+        :name{name},age{age} {}
+    bool operator<(const Personn &rhs) const 
+    {
+        return this->age <rhs.age;
+    }
+    bool operator==(const Personn & rhs) const
+    {
+        return (this->name==rhs.name && this->age == rhs.age);
     }
  };
 
@@ -236,6 +256,103 @@ void test5()
 
 }
 
+void find_test()
+{
+    std::cout<<"=====================FINDTEST========================="<<std::endl;
+    std::vector<int> vec {1,2,3,4,5,6,7,8,9,10};
+    auto loc = std::find(std::begin(vec),std::end(vec),1);
+
+    if (loc != vec.end() )
+        std::cout<<"find in the location of "<< loc-vec.begin()<<std::endl;
+    else 
+        std::cout << "Couldn't find the number" << std::endl;
+
+    std::list<Personn> players {
+        {"Larry", 18},
+        {"Moe", 20},
+        {"Curly", 21}
+    };
+
+    auto loc1 = std::find(players.begin(),players.end(),Personn{"Moe", 20});
+
+    if (loc1 != players.end())
+        std::cout << "Found  Moe" << std::endl;
+    else
+        std::cout << "Moe not found" << std::endl;
+    
+}
+
+void count_test()
+{
+    std::cout<<"=====================COUNTTEST========================="<<std::endl;
+    std::vector<int> vec {1,2,3,4,5,1,2,1};
+    int num = std::count(vec.begin(),vec.end(),2);
+    std::cout << num << " occurrences found" << std::endl;
+
+}
+
+void count_if_test() 
+{
+    std::cout<<"=====================COUNTifTEST========================="<<std::endl;
+    std::vector<int> vec {1,2,3,4,5,1,2,1,100};
+    int num = std::count_if(vec.begin(),vec.end(),[] (int x){return x %2 == 0;});
+    std::cout << num << " even numbers found" << std::endl;
+    num = std::count_if(vec.begin(),vec.end(),[](int x){return x%2 !=0;});
+    std::cout << num << " odd numbers found" << std::endl;
+    num = std::count_if(vec.begin(),vec.end(),[](int x){return x>= 5;});
+    std::cout << num << "  numbers are >= 5" << std::endl;
+
+}
+void replace_test() 
+{
+    std::cout<<"=====================REPLACETEST========================="<<std::endl;
+
+    std::vector<int> vec {1,2,3,4,5,1,2,1};
+
+    for (auto i: vec) {
+         std::cout << i << " ";
+    }
+    std::cout<<std::endl;
+
+    std::replace(vec.begin(),vec.end(),1,100);
+    for (auto i: vec) {
+         std::cout << i << " ";
+    }
+    std::cout<<std::endl;
+
+
+}
+
+void all_of_test()
+{
+    std::cout<<"=====================ALL_OF_TEST========================="<<std::endl;
+    std::vector<int> vec1 {1,3,5,7,9,1,3,13,19,5};
+
+    if (std::all_of(vec1.begin(),vec1.end(),[](int x){return x>10;}))
+        std::cout << "All the elements are > 10" << std::endl;
+    else
+        std::cout << "Not all the elements are > 10" << std::endl;
+    
+    if (std::all_of(vec1.begin(),vec1.end(),[](int x){return x<20;}))
+        std::cout << "All the elements are < 20" << std::endl;
+    else 
+        std::cout << "Not all the elements are < 20" << std::endl;
+
+}
+
+void string_transform_test() 
+{
+    std::cout<<"==================================================="<<std::endl;
+
+    std::string str1 {"This is a test"};
+    std::cout << "Before transform: " << str1 << std::endl;
+    std::transform(str1.begin(),str1.end(),str1.begin(),::toupper );
+    std::cout << "After transform: " << str1 << std::endl;
+
+
+}
+
+
 
 int main ()
 {
@@ -329,6 +446,14 @@ int main ()
     test4();
     test5();
     
+    std::cout<<"==================================================="<<std::endl;
+
+    find_test();
+    count_test();
+    count_if_test();
+    replace_test();
+    all_of_test();
+    string_transform_test();
 
 
     return 0;
