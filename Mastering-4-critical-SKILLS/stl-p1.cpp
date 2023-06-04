57bff0ce06fa4ba5a9f66aa51c58d39d3cc39d23
meysamshahbazi <mysm.shahbazi@gmail.com>
2023-06-04 19:40:39 +0330

    solve stl practice 1

--- Mastering-4-critical-SKILLS/stl-p1.cpp
+++ Mastering-4-critical-SKILLS/stl-p1.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 struct last_k_numbers_sum_stream
 {
     int k;
+    queue<int> m_q;
+    int sum = 0;
     last_k_numbers_sum_stream(int _k) {k = _k;}
+
     int next(int new_num){
-        
-        return 0;
+        if(m_q.size() == k) {
+            sum -= m_q.front();
+            m_q.pop();
+        }
+        sum += new_num;
+        m_q.push(new_num);
+        return sum;
     }
     
 };
